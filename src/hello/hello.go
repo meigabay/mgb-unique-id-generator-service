6366424731b8ef07ad5c1d5a09e56b19e4326c66
wsk <wskddddht@qq.com>
2019-07-29 08:06:59 +0800

    commit

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"github.com/gorilla/mux"
+)
 
 func main() {
+	router := mux.NewRouter()
+	router.HandleFunc("/", HomeHandler)
 	fmt.Printf("hello, world\n")
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprintf(w, "this is home")
 }
