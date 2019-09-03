9c429b9d339233acc828ee7e5d47aa97d40790b8
Hu Xiao <tim.xiao@supernovacompanies.biz>
2019-09-03 17:31:00 +0800

    add hello world

--- src/app/main.go
+++ src/app/main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
-	"fmt"
-	"github.com/gorilla/mux"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "11111",
+		})
+	})
+	r.Run() // listen and serve on 0.0.0.0:8080
 }
 
 
